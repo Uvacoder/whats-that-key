ed0abd56d62b270101adf6bde3bf36349fce27d1
Dominick Jay <dominickjay217@users.noreply.github.com>
2020-05-12 09:49:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const keycode = document.getElementsByTagName("body")[0];
 
 function checkWord(e){
 		const tag = document.querySelector(".tag");
-		tag.innerHTML = e.keyCode;
+		const tagCode = document.querySelector(".tag--code");
+		const taglocation = document.querySelector(".tag--location");
+		const keyCodes = document.querySelector(".keycodes");
+		keyCodes.style.opacity = "1";
+		tag.innerHTML = e.key;
+		tagCode.innerHTML = e.keyCode;
+		taglocation.innerHTML = e.location;
 }
 
 keycode.addEventListener("keydown",checkWord);
